8b1da5b2a46b5f48c54ac8ee688831de257ebefa
huangwei <huangwei@huangwei-Aspire-4745G.(none)>
2012-08-09 20:15:41 +0800

    none

--- src/SysTime.c
+++ src/SysTime.c
@@ -18,6 +18,7 @@ static void _update()
 	struct tm _tm;
 	gmtime_r(&_now, &_tm);
 	snprintf(stm->str[index],64,"[%04d-%02d-%02d %02d:%02d:%02d]",_tm.tm_year+1900,_tm.tm_mon+1,_tm.tm_mday,_tm.tm_hour,_tm.tm_min,_tm.tm_sec);	
+	
 	stm->current_index  = index;
 }
 
@@ -37,6 +38,7 @@ void init_system_time(uint32_t sleep_time)
 		stm = calloc(1,sizeof(*stm));
 		stm->current_index = 1;
 		stm->sleep_time = sleep_time;
+		_update();
 		_update();
 		CREATE_THREAD_RUN(0,_thread_routine,NULL);
 	}
