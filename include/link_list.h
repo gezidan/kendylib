693c87881eebb1bba5b7cd04f21a74db04c1164d
huangwei <huangwei@huangwei-Aspire-4745G.(none)>
2012-10-02 21:46:30 +0800

    removed link_list.c

--- include/link_list.h
+++ include/link_list.h
@@ -5,31 +5,115 @@
 #include "sync.h"
 #include "KendyNet.h"
 #include <stdint.h>
-//typedef struct list_node list_node;
+#include <stdlib.h>
 
-struct link_list;
-//struct block_queue;
+struct link_list
+{
+	int32_t size;
+	list_node *head;
+	list_node *tail;
+	
+};
+
+static inline struct link_list *create_link_list()
+{
+	struct link_list *l = calloc(1,sizeof(*l));
+	return l;
+}
+
+static inline void destroy_link_list(struct link_list **l)
+{
+	free(*l);
+	*l = NULL;
+}
+
+static inline struct list_node* link_list_head(struct link_list *l)
+{
+	return l->head;
+}
 
 
-void link_list_push_back(struct link_list*,list_node*);
+static inline void link_list_swap(struct link_list *to,struct link_list *from)
+{
+	if(from->head && from->tail)
+	{
+		if(to->tail)
+		{
+			to->tail->next = from->head;
+		}else
+		{
+			to->head = from->head;
+		}
+		to->tail = from ->tail;
+		from->head = from->tail = NULL;
+		to->size += from->size;
+		from->size = 0;
+	}
+}
 
-void link_list_push_front(struct link_list*,list_node*);
+static inline void link_list_clear(struct link_list *l)
+{
+	l->size = 0;
+	l->head = l->tail = 0;
+}
 
-list_node* link_list_pop(struct link_list*);
+static inline void link_list_push_back(struct link_list *l,struct list_node *n)
+{
+	if(n->next)
+		return;
+	n->next = 0;
+	if(0 == l->size)
+	{
+		l->head = l->tail = n;
+	}
+	else
+	{
+		l->tail->next = n;
+		l->tail = n;
+	}
+	++l->size;
+}
 
-inline list_node* link_list_head(struct link_list*);
+static inline void link_list_push_front(struct link_list *l,struct list_node *n)
+{
+	if(n->next)
+		return;
+	n->next = 0;
+	if(0 == l->size)
+	{
+		l->head = l->tail = n;
+	}
+	else
+	{
+		n->next = l->head;
+		l->head = n;
+	}
+	++l->size;
 
-inline int32_t link_list_is_empty(struct link_list*);
+}
 
-struct link_list *create_link_list();
+static inline struct list_node* link_list_pop(struct link_list *l)
+{
+	if(0 == l->size)
+		return 0;
+	list_node *ret = l->head;
+	l->head = l->head->next;
+	if(0 == l->head)
+		l->tail = 0;
+	--l->size;
+	ret->next = 0;
+	return ret;
+}
 
-void destroy_link_list(struct link_list**);
+static inline int32_t link_list_is_empty(struct link_list *l)
+{
+	return l->size == 0;
+}
 
-void link_list_clear(struct link_list*);
-
-inline int32_t link_list_size(struct link_list*);
-
-void    link_list_swap(struct link_list*to,struct link_list*from);
+static inline int32_t link_list_size(struct link_list *l)
+{
+	return l->size;
+}
 
 #define LINK_LIST_PUSH_FRONT(L,N) link_list_push_front(L,(list_node*)N)
 
@@ -44,35 +128,5 @@ void    link_list_swap(struct link_list*to,struct link_list*from);
 #define LINK_LIST_DESTROY(L) destroy_link_list(L)
 
 #define LINK_LIST_CLEAR(L) link_list_clear(L)
-
-/*
-struct block_queue* create_block_queue();
-void destroy_block_queue(struct block_queue**);
-void block_queue_push(struct block_queue*,list_node*);
-enum
-{
-	POP_SUCCESS = 0,
-	POP_TIMEOUT = 1,
-	POP_FORCE_WAKE_UP = 2,
-};
-int32_t block_queue_pop(struct block_queue*,list_node**,int32_t timeout);
-void block_queue_force_wakeup(struct block_queue*);
-void block_queue_clear(struct block_queue*);
-
-void block_queue_swap(struct block_queue*bq,struct link_list*from);
-
-
-#define BLOCK_QUEUE_PUSH(L,N) block_queue_push(L,(list_node*)N)
-
-#define BLOCK_QUEUE_POP(L,N,M) block_queue_pop(L,(list_node**)N,M)
-
-#define BLOCK_QUEUE_CREATE() create_block_queue()
-
-#define BLOCK_QUEUE_DESTROY(L) destroy_block_queue(L)
-
-#define BLOCK_QUEUE_CLEAR(L)  block_queue_clear(L)
-
-#define BLOCK_QUEUE_FORCE_WAKEUP(L) block_queue_force_wakeup(L)
-*/
 
 #endif
