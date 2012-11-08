5f6ecebce45ed3addc6d24b94bb19e9e74953cf6
huangwei <huangwei@huangwei-Aspire-4745G.(none)>
2012-11-08 20:16:33 +0800

    调整db_protocal

--- dbcache/frontend/cache_server/cache_server2.c
+++ dbcache/frontend/cache_server/cache_server2.c
@@ -11,9 +11,11 @@
 #include <assert.h>
 #include "common_define.h"
 #include "db_protocal.h"
+#include "allocator.h"
 
 
-allocator_t wpacket_allocator = NULL;
+
+allocator_t rpacket_allocator = NULL;
 uint32_t total_bytes_recv = 0;
 atomic_32_t wpacket_count = 0;
 atomic_32_t rpacket_count = 0; 
@@ -33,25 +35,23 @@ void on_client_disconnect(struct connection *c,int32_t reason)
 
 void on_process_packet(struct connection *c,rpacket_t r)
 {
-	cache_protocal_t p;
 	uint32_t coro_id = rpacket_read_uint32(r);
 	uint8_t type = rpacket_read_uint8(r);
+	wpacket_t ret = NULL;
 	switch(type)
 	{
 		case CACHE_GET:
-			p = create_get();
+			ret = execute_get(gtb,r,coro_id);
 			break;
 		case CACHE_SET:
-			p = create_set();
+			ret = execute_set(gtb,r,coro_id);
 			break;
 		case CACHE_DEL:
-			p = create_del();
+			ret = execute_del(gtb,r,coro_id);
 			break;			
 	}
-	wpacket_t ret = p->execute(gtb,r,coro_id);
 	if(NULL != ret)
 		connection_send(c,ret,NULL);
-	destroy_protocal(&p);
 	rpacket_destroy(&r);
 }
 
@@ -59,6 +59,7 @@ void accept_callback(HANDLE s,void *ud)
 {
 	HANDLE *engine = (HANDLE*)ud;	
 	struct connection *c = connection_create(s,0,SINGLE_THREAD,on_process_packet,on_client_disconnect);
+	c->rpacket_allocator = rpacket_allocator;
 	printf("cli fd:%d\n",s);
 	setNonblock(s);
 	//·¢³öµÚÒ»¸ö¶ÁÇëÇó
@@ -94,8 +95,9 @@ int main(int argc,char **argv)
 		printf("Init error\n");
 		return 0;
 	}
-	wpacket_allocator = (allocator_t)create_block_obj_allocator(SINGLE_THREAD,sizeof(struct wpacket));
-	
+	init_protocal_processer();
+	rpacket_allocator = (allocator_t)create_block_obj_allocator(SINGLE_THREAD,sizeof(struct rpacket));
+		
 	gtb = global_table_create(65536);
 	
 	int32_t i = 0;
