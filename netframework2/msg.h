98ff20c66861799311d036275b2c69ab02e90e28
sniperHW <huangweilook@21cn.com>
2012-12-14 12:07:37 +0800

    none

---
+++ netframework2/msg.h
@@ -0,0 +1,47 @@
+/*	
+    Copyright (C) <2012>  <huangweilook@21cn.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _MSG_H
+#define _MSG_H
+#include "KendyNet.h"
+#include "common_define.h"
+typedef struct msg
+{
+	list_node next;
+	uint8_t   type;
+	uint64_t  usr_data;	
+}*msg_t;
+
+
+static inline msg_t create_msg(uint64_t usr_data,uint8_t type)
+{
+	if(type <= MSG_WPACKET || type >= MSG_END)
+		return NULL;
+	msg_t m = (msg_t)calloc(1,sizeof(*m));
+	m->usr_data = usr_data;
+	m->type = type;
+	return m;	
+}
+
+static inline void  destroy_msg(msg_t *m)
+{
+	free(*m);
+	*m = NULL;
+}
+
+
+#endif
---
+++ netframework2/netservice.c
@@ -0,0 +1,408 @@
+#include "netservice.h"
+#include "msg.h"
+#include "datasocket.h"
+#include "mq.h"
+#include "SysTime.h"
+#include "double_link.h"
+#include "block_obj_allocator.h"
+#include "Connector.h"
+
+extern struct socket_wrapper* GetSocketByHandle(HANDLE);
+extern int32_t      ReleaseSocketWrapper(HANDLE);
+
+
+static int8_t is_init = 0;
+static allocator_t rpacket_allocator = NULL;
+static allocator_t wpacket_allocator = NULL;
+
+#define MQ_SYNC_SIZE 64                 //消息队列冲刷的阀值
+#define SYS_TICKER 5                    //系统时间更新的间隔(单位ms)
+#define ENGINE_RUN_TIME 10               //网络循环的运行时间(单位ms)
+#define WHEEL_TICK 1000                 //时间轮的时间间隔(单位ms)
+
+
+int32_t init_net_service()
+{
+	if(0 == is_init)
+	{
+		
+		if(InitNetSystem() != 0)
+			return -1;
+		is_init = 1;
+		init_system_time(SYS_TICKER);
+		signal(SIGPIPE,SIG_IGN);
+		init_mq_system();		
+		rpacket_allocator = (allocator_t)create_block_obj_allocator(MUTIL_THREAD,sizeof(struct rpacket));
+		wpacket_allocator = (allocator_t)create_block_obj_allocator(MUTIL_THREAD,sizeof(struct wpacket));
+		return 0;
+	}
+	return -1;
+}
+
+wpacket_t    get_wpacket(uint32_t size)
+{
+	return wpacket_create(MUTIL_THREAD,wpacket_allocator,size,0);
+}
+
+wpacket_t    get_wpacket_by_rpacket(rpacket_t r)
+{
+	return wpacket_create_by_rpacket(wpacket_allocator,r);
+}
+
+struct st_connd
+{
+	uint32_t idx;
+	uint32_t timestamp;
+};
+
+static inline struct connection *get_free_connection(struct engine_struct *e,connd_t *connd)
+{
+	uint32_t i = 1;
+	for( ; i < CON_POOL_SIZE; ++i)
+	{
+		if(0 == e->con_pool[i].timestamp)
+		{
+			e->con_pool[i].timestamp = GetSystemMs();
+			struct st_connd *tmp = (struct st_connd*)connd;
+			tmp->idx = i;
+			tmp->timestamp = e->con_pool[i].timestamp;
+			return e->con_pool[i].c;
+		}
+	}
+	return NULL;
+}
+
+static inline struct connection *get_connection(struct engine_struct *e,connd_t connd)
+{
+	struct st_connd *tmp = (struct st_connd*)&connd;
+	if(tmp->idx >= 1 && tmp->idx < CON_POOL_SIZE && tmp->timestamp == e->con_pool[tmp->idx].timestamp)
+		return e->con_pool[tmp->idx].c;
+	return NULL;
+}
+
+static inline void free_connection(struct engine_struct *e,connd_t connd)
+{
+	struct st_connd *tmp = (struct st_connd*)&connd;
+	if(tmp->idx >= 1 && tmp->idx < CON_POOL_SIZE && tmp->timestamp == e->con_pool[tmp->idx].timestamp)
+	{
+		struct connection *c = e->con_pool[tmp->idx].c;
+		ReleaseSocketWrapper(c->socket);
+		wpacket_t w;
+		while(w = LINK_LIST_POP(wpacket_t,c->send_list))
+			wpacket_destroy(&w);
+		buffer_release(&c->unpack_buf);
+		buffer_release(&c->next_recv_buf);
+		c->next_recv_pos = 0;
+		c->unpack_pos = 0;
+		c->unpack_size = 0;
+		c->is_close = 0;
+		c->recv_overlap.isUsed = c->send_overlap.isUsed = 0;
+		if(c->wheelitem)
+			DestroyWheelItem(&(c->wheelitem));
+		e->con_pool[tmp->idx].timestamp = 0;
+	}
+}
+
+
+static void timeout_check(TimingWheel_t t,void *arg,uint32_t now)
+{
+
+	datasocket_t s = (datasocket_t)arg;
+	struct connection *c = get_connection(s->e,s->c);
+	if(NULL == c)
+		return;
+	if(now > c->last_recv && now - c->last_recv >= c->recv_timeout)
+	{
+		//超时了,关闭套接口
+		struct socket_wrapper *sw = GetSocketByHandle(c->socket);
+		if(sw)
+		{
+			double_link_remove((struct double_link_node*)sw);
+			free_connection(s->e,s->c);
+			//ReleaseSocketWrapper(c->socket);	
+			//通知上层，连接超时关闭
+			s->close_reason = -3;
+			s->is_close = 1;
+			msg_t _msg = create_msg((uint64_t)s,MSG_DISCONNECTED);
+			mq_push(s->e->service->mq_out,(list_node*)_msg);
+		}
+	}else
+	{
+		RegisterTimer(t,c->wheelitem,WHEEL_TICK);
+	}
+	
+	//检测是否有发送阻塞
+	wpacket_t w = (wpacket_t)link_list_head(c->send_list);
+	if(w)
+	{
+		if(now > w->send_tick && now - w->send_tick >= c->send_timeout)
+		{
+			//发送队列队首包超过了15秒任然没有发出去,通知上层,发送阻塞
+			msg_t _msg = create_msg((uint64_t)s,MSG_SEND_BLOCK);
+			mq_push(s->e->service->mq_out,(list_node*)_msg);
+		}
+	}
+}
+
+static void on_process_msg(struct engine_struct *e,msg_t _msg)
+{
+	switch(_msg->type)
+	{
+		case MSG_ACTIVE_CLOSE:
+			{
+				datasocket_t s = (datasocket_t)_msg->usr_data;
+				connd_t con = s->c;
+				struct connection *c = get_connection(e,con);
+				if(c)
+					connection_active_close(c);
+			}
+			break;
+		case MSG_NEW_CONNECTION:
+			{
+				HANDLE s = (HANDLE)_msg->usr_data;
+				connd_t con = 0;
+				struct connection *c = get_free_connection(e,&con);
+				if(!c)
+				{
+					ReleaseSocketWrapper(s);
+					printf("empty free connection\n");
+					exit(0);
+					return;
+				}
+				c->rpacket_allocator = rpacket_allocator;
+				c->socket = s;
+				setNonblock(s);
+				datasocket_t data_s = create_datasocket(e,con,e->mq_in);
+				c->usr_data = (uint64_t)data_s;
+				//通知上层，一个新连接到来
+				msg_t _msg = create_msg((uint64_t)data_s,MSG_NEW_CONNECTION);
+				mq_push(e->service->mq_out,(list_node*)_msg);
+				mq_flush();
+				Bind2Engine(e->engine,s,RecvFinish,SendFinish);
+				c->last_recv = GetSystemMs();
+				connection_start_recv(c);
+			}
+			break;
+			
+		case MSG_SET_RECV_TIMEOUT:
+		case MSG_SET_SEND_TIMEOUT:
+			{
+				datasocket_t s = (datasocket_t)_msg->usr_data;
+				struct connection *c = get_connection(e,s->c);
+				if(c)
+				{
+					c->recv_timeout = s->recv_timeout;
+					c->send_timeout = s->send_timeout;
+					if(!c->wheelitem)
+					{
+						c->wheelitem = CreateWheelItem((void*)s,timeout_check,NULL);
+						RegisterTimer(e->timingwheel,c->wheelitem,WHEEL_TICK);
+					}
+				}
+				ref_decrease(&s->_refbase);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+static inline void on_process_send(struct engine_struct *e,wpacket_t w)
+{
+	connd_t con = w->usr_data;
+	struct connection *c = get_connection(e,con);
+	if(NULL == c)
+		wpacket_destroy(&w);
+	else
+		connection_send(c,w,NULL);
+}
+
+static inline void on_process_packet(struct connection *c,rpacket_t r)
+{
+	datasocket_t s = (datasocket_t)c->usr_data;
+	r->usr_data = (uint64_t)s;
+	mq_push(s->e->service->mq_out,(list_node*)r);	
+}
+
+static void on_socket_disconnect(struct connection *c,int32_t reason)
+{
+	datasocket_t s = (datasocket_t)c->usr_data;
+	UnRegisterTimer(c->wheelitem);
+	free_connection(s->e,s->c);
+	if(reason == -1)
+	{
+		
+		//通知上层，连接被动断开
+		s->close_reason = reason;
+		s->is_close = 1;
+		msg_t _msg = create_msg((uint64_t)s,MSG_DISCONNECTED);
+		mq_push(s->e->service->mq_out,(list_node*)_msg);
+	}
+}
+
+
+
+static void *mainloop(void *arg)
+{	
+	printf("start io thread\n");
+	struct engine_struct *e = (struct engine_struct*)arg;
+	uint32_t last_sync = GetCurrentMs();
+	while(0 == e->service->stop)
+	{
+		msg_t _msg = NULL;
+		while(_msg = (msg_t)mq_pop(e->mq_in,0))
+		{
+			if(_msg->type == MSG_WPACKET)
+			{
+				//是一个需要发送的数据包
+				wpacket_t wpk = (wpacket_t)_msg;
+				on_process_send(e,wpk);
+			}
+			else
+			{
+				//处理消息
+				on_process_msg(e,_msg);
+				destroy_msg(&_msg);
+			}
+		}
+		//执行超时检测
+		UpdateWheel(e->timingwheel,GetCurrentMs());
+		//////////
+		EngineRun(e->engine,ENGINE_RUN_TIME);
+		//冲刷mq
+		mq_flush();
+
+	}
+}
+
+
+static void new_connection(netservice_t service,HANDLE s)
+{
+	//随机选择一个engine
+	int32_t index = rand()%service->engine_count;
+	struct engine_struct *e = &(service->engines[index]);
+	msg_t _msg = create_msg((uint64_t)s,MSG_NEW_CONNECTION);
+	mq_push(e->mq_in,(list_node*)_msg);	
+	mq_flush();
+}
+
+static void accept_callback(HANDLE s,void *ud)
+{
+	netservice_t service = (netservice_t)ud;
+	new_connection(service,s);
+}
+
+static void *_Listen(void *arg)
+{
+	netservice_t service = (netservice_t)arg;
+	while(0 == service->stop)
+		acceptor_run(service->_acceptor,100);
+	return NULL;
+}
+
+
+static void mq_item_destroyer(void *ptr)
+{
+	msg_t _msg = (msg_t)ptr;
+	if(_msg->type == MSG_RPACKET)
+		rpacket_destroy((rpacket_t*)&_msg);
+	else if(_msg->type == MSG_WPACKET)
+		wpacket_destroy((wpacket_t*)&_msg);
+	else
+		destroy_msg(&_msg);
+}
+
+netservice_t create_net_service(uint32_t thread_count)
+{
+	if(thread_count == 0)
+		thread_count = 1;
+	if(thread_count > MAX_IO_THREADS)
+		thread_count = MAX_IO_THREADS;
+	
+	netservice_t s = (netservice_t)calloc(1,sizeof(*s));
+	
+	s->engine_count = thread_count;
+	s->mq_out = create_mq(MQ_SYNC_SIZE,mq_item_destroyer);
+	s->thread_listen = create_thread(THREAD_JOINABLE);//joinable
+	s->_acceptor = create_acceptor();
+	
+	uint32_t i = 0;
+	for( ;i < thread_count; ++i)
+	{
+		s->engines[i].mq_in = create_mq(MQ_SYNC_SIZE,mq_item_destroyer);
+		s->engines[i].engine = CreateEngine();
+		s->engines[i].thread_engine = create_thread(THREAD_JOINABLE);//joinable
+		s->engines[i].service = s;
+		s->engines[i].timingwheel = CreateTimingWheel(WHEEL_TICK,MAX_WHEEL_TIME);
+		uint32_t j = 1;
+		for( ; j < CON_POOL_SIZE; ++j)
+			s->engines[i].con_pool[j].c = connection_create(-1,0,MUTIL_THREAD,on_process_packet,on_socket_disconnect);		
+		thread_run(mainloop,&s->engines[i]);//启动线程
+	}
+	thread_run(_Listen,s);//启动listener线程
+	return s;	
+}
+
+void stop_net_service(netservice_t s)
+{
+	if(s->stop == 0)
+	{
+		s->stop = 1;
+		//等待各线程结束
+		thread_join(s->thread_listen);
+		uint32_t i = 0;
+		for( ;i < s->engine_count; ++i)
+			thread_join(s->engines[i].thread_engine);
+	}
+}
+
+void destroy_net_service(netservice_t *s)
+{
+	netservice_t _s = *s;
+	stop_net_service(_s);
+	destroy_acceptor(&_s->_acceptor);
+	destroy_thread(&_s->thread_listen);
+	uint32_t i = 0;
+	for( ;i < _s->engine_count; ++i)
+	{
+		DestroyTimingWheel(&_s->engines[i].timingwheel);
+		destroy_thread(&_s->engines[i].thread_engine);
+		CloseEngine(_s->engines[i].engine);
+		destroy_mq(&_s->engines[i].mq_in);
+	}
+	destroy_mq(&_s->mq_out);
+	free(_s);
+	*s = NULL;
+}
+
+//从网络服务队列中获取到来的消息
+msg_t net_peek_msg(netservice_t s,uint32_t ms)
+{
+	return (msg_t)mq_pop(s->mq_out,ms);
+}
+
+//添加一个网络监听
+HANDLE net_add_listener(netservice_t s,const char *ip,uint32_t port)
+{
+	return add_listener(s->_acceptor,ip,port,accept_callback,s);
+}
+//关闭一个网络监听
+void net_rem_listener(netservice_t s,HANDLE h)
+{
+	rem_listener(s->_acceptor,h);
+}
+
+static void connect_callback(HANDLE s,const char *ip,int32_t port,void*ud)
+{
+	netservice_t service = (netservice_t)ud;
+	new_connection(service,s);	
+}
+
+int32_t net_connect(netservice_t s,const char *ip,uint32_t port)
+{
+	int32_t ret;
+	connector_t con = connector_create();
+	ret = connector_connect(con,ip,port,connect_callback,(void*)s,0);
+	connector_destroy(&con);
+	return ret;	
+}
