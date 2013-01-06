038c62fe825f2ff577dd8c88a620073ecb80aeba
huangwei <huangwei@huangwei-Vostro-230.(none)>
2013-01-06 15:45:31 +0800

    none

--- test/test_syn_st.c
+++ test/test_syn_st.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "util/sync.h"
 
 struct point
 {
@@ -15,7 +16,33 @@ struct point
 
 volatile int get_count = 0;
 volatile int set_count = 0;
+volatile int miss_count = 0;
+mutex_t mtx;
+#define _USE_MTX_
+#ifdef _USE_MTX_
+struct point g_point;
+struct point GetPoint()
+{
+	mutex_lock(mtx);
+	struct point ret;
+	ret.x = g_point.x;
+	ret.y = g_point.y;
+	ret.z = g_point.z;
+	++get_count;
+	mutex_unlock(mtx);
+	return ret;
+}
+void SetPoint(struct point p)
+{
+	mutex_lock(mtx);
+	g_point.x = p.x;
+	g_point.y = p.y;
+	g_point.z = p.z;
+	++set_count;
+	mutex_unlock(mtx);
+}
 
+#else
 struct point *g_point = NULL;
 
 struct point GetPoint()
@@ -45,7 +72,10 @@ struct point GetPoint()
 				}			
 				break;
 			}
+			++miss_count;
 	    }
+	    else
+			++miss_count;
 	}
 	++get_count;
 	return ret;
@@ -55,7 +85,11 @@ void SetPoint(struct point p)
 {
 	static uint32_t g_version = 0;
 	struct point *old_p = g_point;
-	struct point *new_p = calloc(1,sizeof(*new_p));	
+	static struct point  array[2];	
+	static int32_t index  = 0;
+	
+	struct point *new_p = &array[index];
+	index = (index + 1)%2;	
 	new_p->x = p.x;
 	new_p->y = p.y;
 	new_p->z = p.z;
@@ -64,13 +98,10 @@ void SetPoint(struct point p)
 	__sync_synchronize();
 	g_point = new_p;
 	if(old_p)
-	{
 		old_p->version = 0;
-		old_p->x = old_p->y = old_p->z = 0;
-		free(old_p);
-	}
 	++set_count;
 }
+#endif
 
 void *SetRotine(void *arg)
 {
@@ -92,19 +123,22 @@ void *GetRoutine(void *arg)
 
 int main()
 {
+	mtx = mutex_create();	
 	struct point p;
 	p.x = p.y = p.z = 1;
 	SetPoint(p);
 	thread_t t1 = CREATE_THREAD_RUN(1,SetRotine,NULL);
 	thread_t t2 = CREATE_THREAD_RUN(1,GetRoutine,NULL);	
+	//thread_t t3 = CREATE_THREAD_RUN(1,GetRoutine,NULL);
+	//thread_t t4 = CREATE_THREAD_RUN(1,GetRoutine,NULL);								
 	uint32_t tick = GetSystemMs();
 	while(1)
 	{
 		uint32_t new_tick = GetSystemMs();
 		if(new_tick - tick >= 1000)
 		{
-			printf("get:%d,set:%d\n",get_count,set_count);
-			get_count = set_count = 0;
+			printf("get:%d,set:%d,miss:%d\n",get_count,set_count,miss_count);
+			get_count = set_count = miss_count = 0;
 			tick = new_tick;
 		}
 		sleepms(10);
