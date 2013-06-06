4569e58c718117a761bfda13a9302e69add09bd5
sniperHW <huangweilook@21cn.com>
2013-06-06 21:23:19 +0800

    none

---
+++ util/src/uthread_ucontext.h
@@ -0,0 +1,44 @@
+#include <ucontext.h>
+
+struct uthread
+{
+   ucontext_t ucontext;	
+   void *para;   		
+   uthread_t  parent;
+   void* (*main_fun)(void*);
+};
+
+void uthread_main_function(void *arg)
+{
+	uthread_t u = (uthread_t)arg;
+	void *ret = u->main_fun(u->para);
+	if(u->parent)
+		uthread_switch(u,u->parent,ret);
+}
+
+uthread_t uthread_create(uthread_t parent,void*stack,uint32_t stack_size,void*(*fun)(void*))
+{
+	uthread_t u = (uthread_t)calloc(1,sizeof(*u));
+	u->ucontext.uc_stack.ss_sp = stack;
+	u->ucontext.uc_stack.ss_size = stack_size;
+	u->ucontext.uc_link = NULL;
+	u->parent = parent;
+	u->main_fun = fun;
+	getcontext(&(u->ucontext));
+	makecontext(&(u->ucontext),(void(*)())uthread_main_function,1,u);
+	return u;
+}
+
+void uthread_destroy(uthread_t *u)
+{
+	
+}
+
+void*uthread_switch(uthread_t from,uthread_t to,void *para)
+{
+	if(!from)
+		return NULL;
+	to->para = para;
+	swapcontext(&(from->ucontext),&(to->ucontext));
+	return from->para;
+}
