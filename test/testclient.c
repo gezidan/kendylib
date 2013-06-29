de5bfafd1c659ef729ae385a5a7fc56c963a72b2
huangwei <huangwei@huangwei-Vostro-230.(none)>
2013-06-29 22:47:36 +0800

    调整测试用例

--- test/testclient.c
+++ test/testclient.c
@@ -5,61 +5,15 @@
 #include "core/Connector.h"
 #include "core/Connection.h"
 #include "core/common_define.h"
-allocator_t wpacket_allocator = NULL;
+#include "testnet.h"
 
-static int32_t connect_count = 0;
-uint32_t packet_recv = 0;
-uint32_t send_request = 0;
-uint32_t tick = 0;
-uint32_t now = 0;
-#define MAX_CLIENT 1000
-static struct connection *clients[MAX_CLIENT];
-uint32_t last_recv = 0;
-uint32_t ava_interval = 0;
-void init_clients()
-{
-	int32_t i = 0;
-	for(; i < MAX_CLIENT;++i)
-		clients[i] = 0;
-}
-
-void add_client(struct connection *c)
-{
-	int32_t i = 0;
-	for(; i < MAX_CLIENT; ++i)
-	{
-		if(clients[i] == 0)
-		{
-			clients[i] = c;
-			break;
-		}
-	}
-}
-
-void remove_client(struct connection *c,int32_t reason)
-{
-	int32_t i = 0;
-	for(; i < MAX_CLIENT; ++i)
-	{
-		if(clients[i] == c)
-		{
-			clients[i] = 0;
-			break;
-		}
-	}
-	SOCK sock = c->socket;
-	if(0 == connection_destroy(&c))
-	{
-		ReleaseSocketWrapper(sock);
-	}
-}
 
 connector_t con = NULL;
 uint32_t iocp_count = 0;
 
 void on_process_packet(struct connection *c,rpacket_t r)
 {
-	/*uint32_t s = rpacket_read_uint32(r);
+	uint32_t s = rpacket_read_uint32(r);
 	uint32_t t;
 	if(s == (uint32_t)c->socket)
 	{
@@ -67,16 +21,9 @@ void on_process_packet(struct connection *c,rpacket_t r)
 		uint32_t sys_t = GetSystemMs();
 		ava_interval += (sys_t - t);
 		ava_interval /= 2;
-	}*/
+	}
 	++packet_recv;
 	rpacket_destroy(&r);
-	/*wpacket_t wpk = wpacket_create(SINGLE_THREAD,wpacket_allocator,64,0);
-	wpacket_write_uint32(wpk,c->socket);
-	uint32_t sys_t = GetSystemMs();
-	wpacket_write_uint32(wpk,sys_t);
-	wpacket_write_string(wpk,"hello kenny");
-	connection_send(c,wpk,NULL);
-	++send_request;*/
 }
 
 void on_connect_callback(SOCK s,const char *ip,int32_t port,void *ud)
@@ -87,14 +34,14 @@ void on_connect_callback(SOCK s,const char *ip,int32_t port,void *ud)
 	++connect_count;
 	if(s == NULL)
 	{
-		printf("%d,连接到:%s,%d,失败\n",s,ip,port);
+		printf("connet to [%s:%d] failed\n",ip,port);
 	}
 	else
 	{
 
 		setNonblock(s);
 		c = connection_create(s,0,SINGLE_THREAD,on_process_packet,remove_client);
-		printf("%d,连接到:%s,%d,成功\n",s,ip,port);
+		printf("connet to [%s:%d] success\n",ip,port);
 		add_client(c);
 		Bind2Engine(*engine,s,RecvFinish,SendFinish,NULL);
 		wpk = wpacket_create(SINGLE_THREAD,NULL,64,0);
---
+++ test/testnet.h
@@ -0,0 +1,72 @@
+
+#include <assert.h>
+uint32_t packet_recv = 0;
+uint32_t packet_send = 0;
+uint32_t send_request = 0;
+uint32_t tick = 0;
+uint32_t now = 0;
+uint32_t clientcount = 0;
+uint32_t last_send_tick = 0;
+allocator_t wpacket_allocator = NULL;
+uint32_t total_bytes_recv = 0;
+static int32_t connect_count = 0;
+uint32_t last_recv = 0;
+uint32_t ava_interval = 0;
+
+#define MAX_CLIENT 2000
+static struct connection *clients[MAX_CLIENT];
+
+void init_clients()
+{
+	uint32_t i = 0;
+	for(; i < MAX_CLIENT;++i)
+		clients[i] = 0;
+}
+
+void add_client(struct connection *c)
+{
+	uint32_t i = 0;
+	for(; i < MAX_CLIENT; ++i)
+	{
+		if(clients[i] == 0)
+		{
+			clients[i] = c;
+			break;
+		}
+	}
+}
+
+void send2_all_client(rpacket_t r)
+{
+	uint32_t i = 0;
+	wpacket_t w;
+	for(; i < MAX_CLIENT; ++i)
+	{
+		if(clients[i])
+		{
+			w = wpacket_create_by_rpacket(wpacket_allocator,r);
+			assert(w);
+			++send_request;
+			connection_send(clients[i],w,NULL);
+			//connection_push_packet(clients[i],w,NULL);
+		}
+	}
+}
+
+void remove_client(struct connection *c,int32_t reason)
+{
+	uint32_t i = 0;
+	for(; i < MAX_CLIENT; ++i)
+	{
+		if(clients[i] == c)
+		{
+			clients[i] = 0;
+			break;
+		}
+	}
+	SOCK sock = c->socket;
+	if(0 == connection_destroy(&c))
+	{
+		ReleaseSocketWrapper(sock);
+	}
+}
